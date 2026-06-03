1c484d1a00664046f70cfeb1021ab3181468b3e5
lemaitranmedia <lemaitran.media@gmail.com>
2026-06-03 11:59:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/eventoh-checkin/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/main.[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
+  // Thay tên repository của bạn vào đây để tránh lỗi trắng trang khi deploy
+  base: '/eventoh-checkin/', 
 });
